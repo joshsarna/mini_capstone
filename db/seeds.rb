72ef4c3dab1e59d12bcdedf80405ac75f679ab38
joshsarna <joshsarna@gmail.com>
2018-06-26 17:11:34 -0700

    make three Products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.new(name: "The Terminal Man", price: "5.99", description: "paperback book by Michael Crichton")
+product.save
+product = Product.new(name: "A Mad Desire to Dance", price: "25.00", description: "hardcover book by Elie Wiesel")
+product.save
+product = Product.new(name: "The Dark Half", price: "21.95", description: "hardover book by Stephen King")
+product.save
