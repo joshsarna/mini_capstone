80fd39500b320ec0dab8f092a948d3b7127e8c81
joshsarna <joshsarna@gmail.com>
2018-07-01 09:11:31 -0700

    make update action accept user input

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -48,14 +48,40 @@ class Api::ProductsController < ApplicationController
   def update
     @product = Product.find_by(id: params[:id])
 
-    @product.update(
-      name: params[:input_name] || @product.name,
-      price: params[:input_price] || @product.price,
-      description: params[:input_description] || @product.description,
-      format: params[:input_format] || @product.format,
-      condition: params[:input_condition] || @product.condition,
-      image_url: params[:input_image_url] || @product.image_url
-    )
+    answer = "y"
+    while answer == "y" do
+      p "What parameter would you like to update?"
+      update_parameter = gets.chomp
+      p "What would you like to change the #{update_parameter} to?"
+
+      if update_parameter == "name"
+        @product.update(name: gets.chomp)
+      elsif update_parameter == "author"
+        @product.update(author: gets.chomp)
+      elsif update_parameter == "price"
+        @product.update(price: gets.chomp.to_d)
+      elsif update_parameter == "description"
+        @product.update(description: gets.chomp)
+      elsif update_parameter == "format"
+        @product.update(format: gets.chomp)
+      elsif update_parameter == "condition"
+        @product.update(condition: gets.chomp)
+      elsif update_parameter == "image_url"
+        @product.update(image_url: gets.chomp)
+      end
+
+      p "Would you like to update another parameter? [y/n]"
+      answer = gets.chomp
+    end
+
+    # @product.update(
+    #   name: params[:input_name] || @product.name,
+    #   price: params[:input_price] || @product.price,
+    #   description: params[:input_description] || @product.description,
+    #   format: params[:input_format] || @product.format,
+    #   condition: params[:input_condition] || @product.condition,
+    #   image_url: params[:input_image_url] || @product.image_url
+    # )
     @product.save
     render "show.json.jbuilder"
   end
--- runner.rb
+++ runner.rb
@@ -3,3 +3,5 @@ require 'unirest'
 # response = Unirest.delete("localhost:3000/api/products/27")
 
 # response = Unirest.post("localhost:3000/api/products")
+
+response = Unirest.patch("localhost:3000/api/products/28")
