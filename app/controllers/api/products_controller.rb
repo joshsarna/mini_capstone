2e49e301048698f73d9011f9b6be91fe8f49f609
joshsarna <joshsarna@gmail.com>
2018-07-03 19:34:29 -0700

    change create and update actions to accept params not typed into the server

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -11,29 +11,14 @@ class Api::ProductsController < ApplicationController
 
   def create
 
-    p "What is the title of the book you would like to add to the store?"
-    input_name = gets.chomp
-    p "What is the author's name?"
-    input_author = gets.chomp
-    p "How much does the book cost?"
-    input_price = gets.chomp.to_d
-    p "What is the description?"
-    input_description = gets.chomp
-    p "What format is the book?"
-    input_format = gets.chomp
-    p "What condition is the book?"
-    input_condition = gets.chomp
-    p "What is the image url?"
-    input_image_url = gets.chomp
-
     @product = Product.new(
-      name: input_name,
-      price: input_price,
-      description: input_description,
-      author: input_author,
-      format: input_format,
-      condition: input_condition,
-      image_url: input_image_url
+      name: params[:input_name],
+      price: params[:input_price],
+      description: params[:input_description],
+      author: params[:input_author],
+      format: params[:input_format],
+      condition: params[:input_condition],
+      image_url: params[:input_image_url]
     )
     @product.save
     render "show.json.jbuilder"
@@ -48,40 +33,42 @@ class Api::ProductsController < ApplicationController
   def update
     @product = Product.find_by(id: params[:id])
 
-    answer = "y"
-    while answer == "y" do
-      p "What parameter would you like to update?"
-      update_parameter = gets.chomp
-      p "What would you like to change the #{update_parameter} to?"
+    # answer = "y"
+    # while answer == "y" do
+    #   p "What parameter would you like to update?"
+    #   update_parameter = gets.chomp
+    #   p "What would you like to change the #{update_parameter} to?"
 
-      if update_parameter == "name"
-        @product.update(name: gets.chomp)
-      elsif update_parameter == "author"
-        @product.update(author: gets.chomp)
-      elsif update_parameter == "price"
-        @product.update(price: gets.chomp.to_d)
-      elsif update_parameter == "description"
-        @product.update(description: gets.chomp)
-      elsif update_parameter == "format"
-        @product.update(format: gets.chomp)
-      elsif update_parameter == "condition"
-        @product.update(condition: gets.chomp)
-      elsif update_parameter == "image_url"
-        @product.update(image_url: gets.chomp)
-      end
+    @product.update(
+      name: params[:input_name] || @product.name,
+      author: params[:input_author] || @product.author,
+      price: params[:input_price] || @product.price,
+      description: params[:input_description] || @product.description,
+      format: params[:input_format] || @product.format,
+      condition: params[:input_condition] || @product.condition,
+      image_url: params[:input_image_url] || @product.image_url
+    )
 
-      p "Would you like to update another parameter? [y/n]"
-      answer = gets.chomp
-    end
+      # if update_parameter == "name"
+      #   @product.update(name: gets.chomp)
+      # elsif update_parameter == "author"
+      #   @product.update(author: gets.chomp)
+      # elsif update_parameter == "price"
+      #   @product.update(price: gets.chomp.to_d)
+      # elsif update_parameter == "description"
+      #   @product.update(description: gets.chomp)
+      # elsif update_parameter == "format"
+      #   @product.update(format: gets.chomp)
+      # elsif update_parameter == "condition"
+      #   @product.update(condition: gets.chomp)
+      # elsif update_parameter == "image_url"
+      #   @product.update(image_url: gets.chomp)
+      # end
 
-    # @product.update(
-    #   name: params[:input_name] || @product.name,
-    #   price: params[:input_price] || @product.price,
-    #   description: params[:input_description] || @product.description,
-    #   format: params[:input_format] || @product.format,
-    #   condition: params[:input_condition] || @product.condition,
-    #   image_url: params[:input_image_url] || @product.image_url
-    # )
+      # p "Would you like to update another parameter? [y/n]"
+      # answer = gets.chomp
+
+    
     @product.save
     render "show.json.jbuilder"
   end
--- runner.rb
+++ runner.rb
@@ -2,6 +2,16 @@ require 'unirest'
 
 # response = Unirest.delete("localhost:3000/api/products/27")
 
-# response = Unirest.post("localhost:3000/api/products")
+response = Unirest.post("localhost:3000/api/products", parameters: 
+  {
+    name: "book",
+    author: "a",
+    price: "b",
+    description: "c",
+    format: "d",
+    condition: "e",
+    image_url: "f"
+  }
+)
 
-response = Unirest.patch("localhost:3000/api/products/28")
+# response = Unirest.patch("localhost:3000/api/products/28")
