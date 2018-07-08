2fccb92803da76b793c59cdf9a972ec5889a2c91
joshsarna <joshsarna@gmail.com>
2018-07-08 09:42:35 -0700

    make is_discounted?, tax, and total methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price && price >= 5
+      return false
+    else
+      return true
+    end
+  end
+
+  def tax
+    tax = 0.09
+    if price
+      (price * tax).round(2)
+    end
+  end
+
+  def total
+    if price
+      price + tax
+    end
+  end
 end
--- app/views/api/products/index.json.jbuilder
+++ app/views/api/products/index.json.jbuilder
@@ -1,3 +1,9 @@
 json.array! @products.each do |product|
   json.book product
+
+  json.formatted do
+    json.discounted product.is_discounted?
+    json.tax product.tax
+    json.total product.total
+  end
 end
